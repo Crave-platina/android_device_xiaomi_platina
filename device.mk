073ec07aea8104dd73e98eed1a58eac642d67160
mike dooley <mdooley@google.com>
2018-12-18 20:29:02 +0100

    platina: Enabling sound trigger V2_2
    V2_2 has the same functionality as V2_1 except it adds one new
    api, getModelState.
    
    Note: V2_1 will be removed in a subsequent cl.
    
    Test: built android and tested using SoundTriggerTestApp
    
    Bug: 70206501
    Change-Id: Id2647687732e79df252588d69f95d2a407e0092c

--- device.mk
+++ device.mk
@@ -21,7 +21,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio-impl \
     android.hardware.bluetooth.audio@2.0-impl:32 \
-    android.hardware.soundtrigger@2.1-impl:32
+    android.hardware.soundtrigger@2.2-impl:32
 
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor
