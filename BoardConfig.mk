7a6e9a85b33a46917cd50f4abf9bb59eb6bbc9a3
LuK1337 <priv.luk@gmail.com>
2019-04-16 01:19:19 +0200

    platina: Set TW_NO_HAPTICS to true
    Change-Id: Icbf069f6293f54b00caf430e9e9479ed00bc9b2c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -134,6 +134,7 @@ TW_INCLUDE_NTFS_3G := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_THEME := portrait_hdpi
 TW_USE_TOOLBOX := true
+TW_NO_HAPTICS := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_IGNORE_MISC_WIPE_DATA := true
 TW_Y_OFFSET := 80
