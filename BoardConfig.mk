28c67599c2209a59d1184f21f80d5713a05d7d82
rany <ranygh@riseup.net>
2022-07-08 08:23:48 +0300

    platina: Set TW_NO_CPU_TEMP
    Change-Id: Ie7b2cdfc853deb1bd5c1a5436d015482bc96fc94

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -135,6 +135,7 @@ TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_THEME := portrait_hdpi
 TW_USE_TOOLBOX := true
 TW_NO_HAPTICS := true
+TW_NO_CPU_TEMP := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_IGNORE_MISC_WIPE_DATA := true
 TW_Y_OFFSET := 80
