e6e29008c2ce4f3811b0243081f452b71a5b9122
Bruno Martins <bgcngm@gmail.com>
2024-06-23 12:29:15 +0100

    platina: Move to new RFS install_symlink targets
    Change-Id: I9560fd0da00958d9349a0881b5a5f64a2c02e495

--- device.mk
+++ device.mk
@@ -6,6 +6,9 @@
 
 DEVICE_PATH := device/xiaomi/platina
 
+# Add common definitions for Qualcomm
+$(call inherit-product, hardware/qcom-caf/common/common.mk)
+
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService-Soong \
