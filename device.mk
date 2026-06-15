71f84b46cd7f92a97909d9cb1a8d0a10890c340e
hanifardhani <hanif.ardhani98@gmail.com>
2026-06-16 01:08:47 +0700

    platina: Build Aperture camera app
    Change-Id: I791ec261f7ad900ade1cc5c99a232c519e9e5ee9

--- device.mk
+++ device.mk
@@ -89,6 +89,9 @@ PRODUCT_PACKAGES += \
     libMegviiFacepp-0.5.2 \
     libmegface
 
+PRODUCT_PACKAGES += \
+    Aperture
+
 # Component overrides
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
